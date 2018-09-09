be4ab23df373208751a56beafabfc988ff74dcdd
BadStar <2mh.mh1148@gmail.com>
2018-09-09 23:41:40 +0300

    Initial commit

---
+++ src/com/example/java1/java1.java
@@ -0,0 +1,10 @@
+package com.example.java1;
+
+public class java1 {
+
+    public static void main(String[] arqs) {
+        System.out.println("\nМихайлов М.А.");
+        System.out.println("\nИКС АТ-161");
+        System.out.println("\nКонтракт");
+    }
+}
